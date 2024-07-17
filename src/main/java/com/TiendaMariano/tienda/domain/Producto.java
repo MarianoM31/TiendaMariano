ff8234decb92cea566a6e819f38320be887b4713
MarianoM31 <147455611+MarianoM31@users.noreply.github.com>
2024-07-16 21:16:51 -0600

    Avance practica en clase

--- src/main/java/com/TiendaMariano/tienda/domain/Producto.java
+++ src/main/java/com/TiendaMariano/tienda/domain/Producto.java
@@ -5,14 +5,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.io.Serializable;
 import lombok.Data;
 
-@Data //automaticamente crear set/get
+@Data
 @Entity
 @Table(name = "producto")
-
 public class Producto implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -27,20 +28,20 @@ public class Producto implements Serializable {
     private int existencias;
     private String rutaImagen;
     private boolean activo;
-    private Long idCategoria;
+
+    @ManyToOne
+    @JoinColumn(name = "id_categoria")
+    Categoria categoria;
 
     public Producto() {
     }
 
-    public Producto(Long idProducto, String descripcion, String detalle, double precio, int existencias, String rutaImagen, boolean activo, Long idCategoria) {
-        this.idProducto = idProducto;
+    public Producto(String descripcion, String detalle, double precio, int existencias, String imagen, boolean activo) {
         this.descripcion = descripcion;
         this.detalle = detalle;
         this.precio = precio;
         this.existencias = existencias;
-        this.rutaImagen = rutaImagen;
+        this.rutaImagen = imagen;
         this.activo = activo;
-        this.idCategoria = idCategoria;
     }
-    
 }

2e5330c5a977423adc00ba7dd2d5be353daa7aac
MarianoM31 <147455611+MarianoM31@users.noreply.github.com>
2024-07-30 23:09:08 -0600

    Avance practica en clase

---
+++ src/main/java/com/TiendaMariano/tienda/dao/RolDao.java
@@ -0,0 +1,9 @@
+package com.TiendaMariano.tienda.dao;
+
+import com.TiendaMariano.tienda.domain.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolDao extends JpaRepository<Rol, Long> {
+
+    }
+
